776a83116c252fc24b6bdb522120185262ff573e
BenNG <benjamin.kim.nguyen@gmail.com>
2016-01-21 17:13:22 +0100

    Update 2016-01-21T16:13:17.584Z

--- bundle.js
+++ bundle.js
@@ -1,5 +1,30 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.BurnerTimer = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (global){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var drawCoord = function drawCoord(x, radius, degrees) {
+    var offset = x - radius;
+
+    var radians = degrees * Math.PI / 180;
+
+    var rX = radius + offset + Math.sin(radians) * radius;
+    var rY = radius + offset - Math.cos(radians) * radius;
+
+    if (!rX || !rY) {
+        return '';
+    }
+
+    var dir = degrees > 180 ? 1 : 0;
+
+    var coord = 'M' + (radius + offset) + ',' + (radius + offset) + ' ' + 'L' + (radius + offset) + ',' + offset + ' ' + 'A' + radius + ',' + radius + ' 0 ' + dir + ',1 ' + rX + ',' + rY;
+
+    return coord;
+};
+
+module.exports = {
+    drawCoord: drawCoord
+};
+
+},{}],"burner-timer":[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -8,7 +33,7 @@ Object.defineProperty(exports, '__esModule', {
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-var _react = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null);
+var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
@@ -80,32 +105,4 @@ var BurnerTimer = _react2['default'].createClass({
 exports['default'] = BurnerTimer;
 module.exports = exports['default'];
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./utils/timer":2,"react-dom":undefined}],2:[function(require,module,exports){
-'use strict';
-
-var drawCoord = function drawCoord(x, radius, degrees) {
-    var offset = x - radius;
-
-    var radians = degrees * Math.PI / 180;
-
-    var rX = radius + offset + Math.sin(radians) * radius;
-    var rY = radius + offset - Math.cos(radians) * radius;
-
-    if (!rX || !rY) {
-        return '';
-    }
-
-    var dir = degrees > 180 ? 1 : 0;
-
-    var coord = 'M' + (radius + offset) + ',' + (radius + offset) + ' ' + 'L' + (radius + offset) + ',' + offset + ' ' + 'A' + radius + ',' + radius + ' 0 ' + dir + ',1 ' + rX + ',' + rY;
-
-    return coord;
-};
-
-module.exports = {
-    drawCoord: drawCoord
-};
-
-},{}]},{},[1])(1)
-});
+},{"./utils/timer":1,"react":undefined,"react-dom":undefined}]},{},[]);
---
+++ example.js
@@ -0,0 +1,31 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var BurnerTimer = require('burner-timer');
+
+var App = React.createClass({
+	displayName: 'App',
+
+	render: function render() {
+
+		var timerProps = {
+			totalTime: 20 * 1000, // ms
+			consumedTime: 2000, // ms
+			progressColor: '#5c6bc0',
+			size: 120
+		};
+
+		/*textToDisplay: 'BlaBla',*/
+		return React.createElement(
+			'div',
+			null,
+			React.createElement(BurnerTimer, timerProps)
+		);
+	}
+});
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
+
+},{"burner-timer":undefined,"react":undefined,"react-dom":undefined}]},{},[1]);
